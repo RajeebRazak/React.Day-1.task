f6068b5221ed698e5e95beee910c12fd48e1d312
RajeebRazak <rajeebrazak786@gmail.com>
2023-11-08 21:14:15 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
+ import Cards from './card.jsx'
+ 
+ function App() {
+  
+  const PlanDetails = [
+     { 
+       planName:"free" ,
+       price:"0" ,
+       features :[
+        {
+           name :"Single User",
+           enable : true 
+        },
+        {
+           name :"5GB Storage" ,
+           enable : true 
+        },
+        {
+           name :"Unlimited Public Project" ,
+           enable : true 
+        },
+        {
+           name :"Community Acess" ,
+           enable : true 
+        },
+        {
+           name :"Unlimmited Private Projects" ,
+           enable : false
+        },
+        {
+           name :"Dedicated Phone Support" ,
+           enable : false
+        },
+        { 
+           name :"Free Subdomain" ,
+           enable : false
+        },
+        {
+          name :"Monthly Status Reports" ,
+          enable : false
+       },
+       ]
+       
+     },
+     {
+      planName:"Plus" ,
+      price:"9" ,
+      features :[
+        {
+           name :"5 User" ,
+           enable : true 
+        },
+        {
+           name :"5GB Storage" ,
+           enable : true 
+        },
+        {
+           name :"Unlimited Public Project" ,
+           enable : true 
+        },
+        {
+           name :"Community Acess" ,
+           enable : true 
+        },
+        {
+           name :"Unlimmited Private Projects" ,
+           enable : true 
+        },
+        {
+           name :"Dedicated Phone Support" ,
+           enable : true 
+        },
+        {
+           name :"Free Subdomain" ,
+           enable : true 
+        },
+        {
+          name :"Monthly Status Reports" ,
+          enable : false
+       },
+       ]
+     },
+     {
+      planName:"Pro" ,
+      price:"49" ,
+      features :[
+        {
+           name :"Unlimited User" ,
+           enable : true 
+        },
+        {
+           name :"5GB Storage" ,
+           enable : true 
+        },
+        {
+           name :"Unlimited Public Project" ,
+           enable : true 
+        },
+        {
+           name :"Community Acess" ,
+           enable : true 
+        },
+        {
+           name :"Unlimmited Private Projects" ,
+           enable : true 
+        },
+        {
+           name :"Dedicated Phone Support" ,
+           enable : true 
+        },
+        {
+           name :"Free Subdomain" ,
+           enable : true 
+        },
+        {
+          name :"Monthly Status Reports" ,
+          enable : true 
+       },
+       ]
+     }
+  ];
+return (
+    
+    <section class="pricing py-5">
+     <div className="container">
+     <div className="row">
+      {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        PlanDetails.map((detail) => {
+           return <Cards details={detail}></Cards>
+        })
+      }
+
+
+
+      
+  
     </div>
+  </div>
+  </section>
+
+
+
+
+
   );
 }
 
